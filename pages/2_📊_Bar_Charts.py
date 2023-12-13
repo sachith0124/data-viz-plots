07bf6aa1031bd7fd6a437a299ea5f1c32e5b29a1
Sachith Janjirala <sachith.janjirala@gmail.com>
2023-12-12 22:40:45 -0600

    added sidebar imports in all the pages

--- pages/2_📊_Bar_Charts.py
+++ pages/2_📊_Bar_Charts.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import collections
-import numpy as np
 
 import sys
 sys.path.append('../')
---
+++ pages/5_🗺️_Geospatial_Charts.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+import sys
+sys.path.append('../')
+from util import set_sidebar
+from config import plt
+
+set_sidebar()
