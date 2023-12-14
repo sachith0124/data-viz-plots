5336c4a836ada6f9025293d35cb6c394179e5df9
Sachith Janjirala <sachith.janjirala@gmail.com>
2023-12-13 19:28:54 -0600

    added config to wide layout

--- 1_🏠_App_Home.py
+++ 1_🏠_App_Home.py
@@ -1,4 +1,6 @@
 import streamlit as st
 import util
 
+st.set_page_config(layout='wide')
+
 util.set_sidebar()
--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 from matplotlib import pyplot as plt
 
-st.set_page_config(layout='wide')
-
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
