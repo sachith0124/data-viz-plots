b0b827a001324aa642e3553b5b8b0b93291469c9
Sachith Janjirala <sachith.janjirala@gmail.com>
2024-01-02 16:59:27 -0600

    refractored code in barcharts-notebook

--- 1_🏠_App_Home.py
+++ 1_🏠_App_Home.py
@@ -6,8 +6,17 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON, layout="wide")
 
 util.set_sidebar()
 
-st.title("Data Vizualization Projects")
-util.add_section_title("A set of data vizualization mini projects showcased as a Streamlit app.", level=3)
-st.write("**Statistical Analysis, Data Analysis, Data Visualizaition, Interactive Dashboards**")
-st.write("using Pandas, Numpy, IPython, Maplotlib, Seaborn, Vega-Altair, Streamlit")
-st.header('', divider='blue')
+header = st.container()
+header.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
+
+header.title("Data Vizualization Projects")
+header.markdown("### A set of data vizualization mini projects showcased as a Streamlit app.")
+header.write("**Statistical Analysis, Data Analysis, Data Visualizaition, Interactive Dashboards**")
+header.write("using Pandas, Numpy, IPython, Maplotlib, Seaborn, Vega-Altair, Streamlit")
+header.header('', divider='blue')
+
+st.write('\n\n')
+st.subheader("Overview")
+st.write("---")
+
+st.markdown("##### Bar Charts:")
